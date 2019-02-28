78ae12947fdd40bf7df110a99a0f9e120a0f805c
Jonas Kipfstuhl <jonas.kipfstuhl@t-online.de>
2019-02-28 21:28:46 +0100

    Emacs: imporve company-clang
    company-clang insterts templates after completion by default, this is
    not what I want. So for now I use a wrapper company backend that
    prevents the template creation.

---
+++ emacs/.emacs.d/company-my-clang.el
@@ -0,0 +1,15 @@
+(require 'cl)
+;; (require 'comany-clang)
+
+(defun company-my-clang (command &optional arg &rest ignored)
+  ""
+  (interactive (list 'interactive))
+  (cl-case command
+    (interactive (comapny-begin-backend 'company-my-clang))
+    (init (company-clang command arg ignored))
+    (prefix (company-clang command arg ignored))
+    (candidates (company-clang command arg ignored))
+    (meta (company-clang command arg ignored))
+    (annotation (company-clang command arg ignored))))
+
+(provide 'company-my-clang)
